01f6eb660e649c17d861a8277a61483a092c9764
surru21 <69420007+surru21@users.noreply.github.com>
2020-08-31 17:30:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, redirect, url_for, render_template
+import pandas as pd
+import numpy as np
+#from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+import pickle
+
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+tfidf=pickle.load(open('transform.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+        return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+     if request.method == 'POST':
+         message = request.form['message']
+         data = [message]
+         vect = tfidf.trasform(data).toarray()
+         my_prediction = clf.predict(vect)
+     return render_template('result.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+     app.run(debug=True, use_reloader=False)  
