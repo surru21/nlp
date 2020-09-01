7fe6626f830d921caa03cdcb6cd4b5b3520c1145
surru21 <69420007+surru21@users.noreply.github.com>
2020-09-01 11:34:50 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -18,12 +18,12 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-     if request.method == 'POST':
-         message = request.form['message']
-         data = [message]
-         vect = tfidf.trasform(data).toarray()
-         my_prediction = clf.predict(vect)
-     return render_template('result.html',prediction = my_prediction)
+       if request.method == 'POST':
+       message = request.form['message']
+       data = [message]
+       vect = tfidf.transform(data).toarray()
+       my_prediction = clf.predict(vect)
+       return render_template('result.html',prediction = my_prediction) 
 
 if __name__ == '__main__':
      app.run(debug=True, use_reloader=False)  
