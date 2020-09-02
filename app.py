54a737e91362db3c2fb7f8c67ce9d5d9e225a80f
surru21 <69420007+surru21@users.noreply.github.com>
2020-09-02 20:39:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 import pickle
-import nltk
-nltk.download('punkt')
-nltk.download('wordnet')
-from gensim.parsing.preprocessing import STOPWORDS
-from nltk.tokenize import word_tokenize
-from string import punctuation
 
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
@@ -27,30 +21,7 @@ def predict():
        if request.method == 'POST':
          message = request.form['message']
          data = [message]
-         all_stopwords_gensim = STOPWORDS
-         all_punct = set(punctuation)
-         all_stopwords_gensim1 = STOPWORDS.union(set(all_punct))                                                           
-         all_stopwords_gensim2 = all_stopwords_gensim1.union(set(['hello','cc','sir', 'hi', 'hallo','lady','gentleman','man','hey', 'received from', 'please']))                    
-         sw_list = {"cant","can't","cannot","not","@"}                                                 
-         all_stopwords_gensim = all_stopwords_gensim2.difference(sw_list)
-         data=str(data)
-         data=data.lower()                              
-         data=data.replace("_"," ")          
-         data=data.replace("-"," ")          
-         data=data.replace("#"," ")          
-         data=data.replace("/"," ")          
-         data=data.replace(":"," ")  
-         data1 = word_tokenize(data) 
-         data2 = [word for word in data1 if not word in all_stopwords_gensim]
-         data2 = str(data2)
-         data3 = data2.replace(r'\d', '')              
-         data3 = data3.replace(r'\S*@\S*\s?', '')      
-         data3 = data3.replace(r'[^\x00-\x7f]+', '')                             
-         data3 = data3.replace(",","")
-         data3 = data3.replace("'","")
-         data3 = data3.replace("[","")
-         data3 = data3.replace("]","")
-         vect = tfidf.transform(data3).toarray()
+         vect = tfidf.transform(data).toarray()
          my_prediction = clf.predict(vect)
        return render_template('result.html',prediction = my_prediction) 
 
